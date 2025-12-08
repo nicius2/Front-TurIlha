068ea5e2007e386e1d1ddc19c255a3ea359d073f
nicius2 <marco.marks06@gmail.com>
2025-12-08 14:28:21 -0300

    config: configurando as rotas na vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: 'build',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
